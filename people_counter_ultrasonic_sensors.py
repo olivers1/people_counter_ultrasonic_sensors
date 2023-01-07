31535f89ed9d192a0e26f5de9fa633a544bce8de
olivers1 <oliver.staberg@gmail.com>
2023-01-07 22:21:09 +0100

    refactoring code, detect_motion func needs to be refactored

--- people_counter_ultrasonic_sensors.py
+++ people_counter_ultrasonic_sensors.py
@@ -13,6 +13,13 @@ SENSOR_2_ECHO = 27
 N_SAMPLES = 6
 SAMPLE_WAIT = 0.07
 
+# calibration
+N_CALIBRATION_SAMPLES = 6
+SENSOR_DISTANCE_VARIATION = 2
+
+# trigg threshold
+DISTANCE_TRESHOLD = 5
+
 # status enums
 class Sensor_1_State(Enum):
     NO_TRIGG = 0
@@ -21,6 +28,11 @@ class Sensor_1_State(Enum):
 class Sensor_2_State(Enum):
     NO_TRIGG = 0
     TRIGG = 1
+
+class Sensor_Calibration_Sts(Enum):
+    UNKNOWN = 0
+    CALIBRATED = 1
+    CALIBRATION_FAILED = 2
 
 class FirstSensorTriggSts(Enum):
     UNKNOWN = 0
@@ -52,16 +64,27 @@ class UltrasonicSensor:
     def __init__(self, trig_pin, echo_pin, n_samples=11, sample_wait=0.1):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)  # use GPIO.BOARD for board pin values
-        self._ref_distance_sensor = 0
+        self._sensor_1_value = 0
+        self._sensor_2_value = 0
+        # initiate status enums
+        self._sensor_1_state = Sensor_1_State.NO_TRIGG
+        self._sensor_2_state = Sensor_2_State.NO_TRIGG 
+        self._sensor_calibration_sts = Sensor_Calibration_Sts.UNKNOWN
+        self._first_sensor_trigg_sts = FirstSensorTriggSts.UNKNOWN
+        self._second_sensor_trigg_sts = SecondSensorTriggSts.UNKNOWN
+        self._first_sensor_untrigg_sts = FirstSensorUntriggSts.UNKNOWN
+        self._second_sensor_untrigg_sts = SecondSensorUntriggSts.UNKNOWN 
+        self._detected_scenario = DetectedScenario.NO_SCENARIO
+
         self._trig_pin = trig_pin
         self._echo_pin = echo_pin
         self._sensor_obj = sensor.Measurement(self._trig_pin, self._echo_pin)
         print("constructor")
 
-    def calibrate(self, n_cal_samples, mean_difference):
+    def calibrate(self):
         sensor_values = []
 
-        for _ in range(n_cal_samples):
+        for _ in range(N_CALIBRATION_SAMPLES):
             sensor_values.append(self.read_distance())
 
         # print calibration read-outs
@@ -69,41 +92,75 @@ class UltrasonicSensor:
 
         # evaluate calibration
         sensor_mean = sum(sensor_values) / len(sensor_values)
-        arr = np.asarray(sensor_values)     # convert list to array for numpy package
+        arr = np.asarray(sensor_values)     # convert list to array with numpy package
 
-        # add reference distance value that is closest to mean value from the calibration of the sensor
+        # assign closest readout value as reference distance value
         index = (np.abs(arr - sensor_mean)).argmin()
-        self._ref_distance_sensor = sensor_values[index]
+        ref_distance_sensor = sensor_values[index]
         
         # verify if measured data is ok by comparing mean value with selected reference value
-        if self._ref_distance_sensor - sensor_mean > mean_difference:
+        if ref_distance_sensor - sensor_mean > SENSOR_DISTANCE_VARIATION:
+            self._sensor_calibration_sts = Sensor_Calibration_Sts.CALIBRATION_FAILED
             raise ValueError("no stable reference data")
-
-
-
+        else:
+            self._sensor_calibration_sts = Sensor_Calibration_Sts.CALIBRATED
+        
+        return ref_distance_sensor
 
     def read_distance(self):
         return round(self._sensor_obj.raw_distance(N_SAMPLES, SAMPLE_WAIT), 1)
+
+    def detect_sensor_trigg(self, sensor_1, sensor_2):
+        # read sensors
+        self._sensor_1_value = sensor_1.read_distance()
+        self._sensor_2_value = sensor_2.read_distance() 
+        if abs(self._sensor_1_value - self._ref_distance_sensor_1) > DISTANCE_TRESHOLD:
+            self._sensor_1_state = Sensor_1_State.TRIGG
+        elif abs(self._sensor_2_value - self._ref_distance_sensor_2) > DISTANCE_TRESHOLD:
+            self._sensor_2_state = Sensor_2_State.TRIGG
+        else:
+            self._sensor_1_state = Sensor_1_State.NO_TRIGG
+            self._sensor_2_state = Sensor_2_State.NO_TRIGG
+    
+    def detect_motion(self, sensor_1, sensor_2):
+        # detect first and second sensor triggs
+        if self._sensor_calibration_sts == Sensor_Calibration_Sts.CALIBRATED and self._first_sensor_trigg_sts == FirstSensorTriggSts.UNKNOWN:
+            while self._first_sensor_trigg_sts == FirstSensorTriggSts.UNKNOWN and self._second_sensor_trigg_sts == SecondSensorTriggSts.UNKNOWN:
+                self.detect_sensor_trigg(sensor_1, sensor_2)
+                if self._sensor_1_state == Sensor_1_State.TRIGG:
+                    self._first_sensor_trigg_sts = FirstSensorTriggSts.SENSOR_1
+                    if self._sensor_2_state == Sensor_2_State.TRIGG:
+                        self._second_sensor_trigg_sts = SecondSensorTriggSts.SENSOR_2
+                elif self._sensor_2_state == Sensor_2_State.TRIGG:
+                    self._first_sensor_trigg_sts = FirstSensorTriggSts.SENSOR_2
+                    if self._sensor_1_state == Sensor_1_State.TRIGG:
+                        self._second_sensor_trigg_sts = SecondSensorTriggSts.SENSOR_1
+                print("first sensor trigg:", self._first_sensor_trigg_sts.name)
+                print("second sensor trigg:", self._second_sensor_trigg_sts.name)
+        #elif self._first_sensor_trigg_sts != FirstSensorTriggSts.UNKNOWN and self._second_sensor_trigg_sts != FirstSensorTriggSts.UNKNOWN
+
+        
+
 
 
 class SensorHandler(UltrasonicSensor):
     def __init__(self):
         # setup and reset enums
-        sensor_1_state = Sensor_1_State.NO_TRIGG
-        sensor_2_state = Sensor_2_State.NO_TRIGG 
-        first_sensor_trigg_sts = FirstSensorTriggSts.UNKNOWN
-        second_sensor_trigg_sts = SecondSensorTriggSts.UNKNOWN
-        first_sensor_untrigg_sts = FirstSensorUntriggSts.UNKNOWN
-        second_sensor_untrigg_sts = SecondSensorUntriggSts.UNKNOWN 
-        detected_scenario = DetectedScenario.NO_SCENARIO
+        
 
         # create sensor objects
         self._sensor_1 = UltrasonicSensor(SENSOR_1_TRIG, SENSOR_1_ECHO, N_SAMPLES, SAMPLE_WAIT)
         self._sensor_2 = UltrasonicSensor(SENSOR_2_TRIG, SENSOR_2_ECHO, N_SAMPLES, SAMPLE_WAIT)
 
         # calibrate sensors
-        self._sensor_1.calibrate(10, 2)
-        self._sensor_2.calibrate(10, 2) 
+        self._ref_distance_sensor_1 = self._sensor_1.calibrate()
+        self._ref_distance_sensor_2 = self._sensor_2.calibrate() 
+        print("ref_distance_sensor_1:", self._ref_distance_sensor_1)
+        print("ref_distance_sensor_2:", self._ref_distance_sensor_2)
+
+        # detect motion
+        self._sensor_1.detect_motion(self._sensor_1, self._sensor_2)
+ 
 
 
 
